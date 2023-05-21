e6c65f33829bfd63415c0697f9ed76590c66cac1
OksanaDrobakha <6249784@gmail.com>
2023-05-21 23:13:05 +0400

    done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 'use strict';
 
 new Swiper('.image-slider', {
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
