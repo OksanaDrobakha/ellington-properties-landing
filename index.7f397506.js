159d55c253e722f71516a1c2b2d0bd57cf70a36e
OksanaDrobakha <111773558+OksanaDrobakha@users.noreply.github.com>
2023-05-21 18:10:17 +0000

    Deploying to gh-pages from @ OksanaDrobakha/ellington-properties-landing@29fed8fce1ac97951e4fd447f1a09c9c3c59c0f7 🚀

--- index.7f397506.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".image-slider",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0},hashNavigation:{watchState:!0},keyboard:{enabled:!0,onlyInViewport:!0},slidesPerView:3,spaceBetween:51,zoom:{maxRatio:3,minRatio:1}});
-//# sourceMappingURL=index.7f397506.js.map
